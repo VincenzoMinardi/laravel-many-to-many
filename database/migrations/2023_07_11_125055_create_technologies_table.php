a27b4b946411d6661688a9298a853d0f21793129
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-11 17:52:06 +0200

    fix sintax

--- database/migrations/2023_07_11_125055_create_technologies_table.php
+++ database/migrations/2023_07_11_125055_create_technologies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
-            $table->string('techonology', 50);
+            $table->string('technology', 50);
         });
     }
 
