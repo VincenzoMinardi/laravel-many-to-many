220569e888c02e772348a887c1db60a7d35852a7
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-11 15:30:45 +0200

    add string techology

--- database/migrations/2023_07_11_125055_create_technologies_table.php
+++ database/migrations/2023_07_11_125055_create_technologies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
-            $table->string('techonoly', 50);
+            $table->string('techonology', 50);
         });
     }
 
