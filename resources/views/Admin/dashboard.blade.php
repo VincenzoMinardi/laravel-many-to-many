e62d5339df7bbcbf2f7c68d8170c5ab56382e962
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-11 14:48:01 +0200

    fist gommit

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.layouts.base')
+
+@section('contents')
+<div class="container">
+    <p class="text-center h1">Qui troverai tutti miei progetti riguardo al mio percorso fatto con Boolean</p>
+    <div class="text-center">
+
+        <button class="btn-dashboard">
+            <a class="color" href="http://127.0.0.1:8000/admin/projects">Clicca Qui</a>
+        </button>
+           
+    </div>
+</div>
+    
+    
+</div>
+
+@endsection
