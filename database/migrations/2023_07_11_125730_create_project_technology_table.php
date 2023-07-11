906d22769bc02373260bd866d34762b86b91f30a
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-11 16:18:15 +0200

    fix error project_id

--- database/migrations/2023_07_11_125730_create_project_technology_table.php
+++ database/migrations/2023_07_11_125730_create_project_technology_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->unsignedBigInteger('projects_id');
+            $table->unsignedBigInteger('project_id');
             $table->unsignedBigInteger('technology_id');
 
             $table->foreign('project_id')->references('id')->on('projects');
