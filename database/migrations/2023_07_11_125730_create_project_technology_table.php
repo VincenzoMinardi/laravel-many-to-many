284abc1398b0b5f1fdbaa4e4eb98c6af2c427202
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-11 15:15:07 +0200

    add function unsignedBigInteger

--- database/migrations/2023_07_11_125730_create_project_technology_table.php
+++ database/migrations/2023_07_11_125730_create_project_technology_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('project_technology', function (Blueprint $table) {
+            $table->unsignedBigInteger('projects_id');
+            $table->unsignedBigInteger('technology_id');
+
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('technology_id')->references('id')->on('technologies');
         });
     }
 
