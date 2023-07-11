6dd44b98003f0fe4af071ee9b6ecb99d589f5b0f
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-11 16:18:27 +0200

    import class seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            TechnologiesTableSeeder::class,
             TypesTableSeeder::class,
             ProjectsTableSeeder::class,
+
 
         ]);
     }
