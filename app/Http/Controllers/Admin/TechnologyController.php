75c93458f4701fce0acfd29f85bdd6ab7e38f43c
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-12 15:40:08 +0200

    fix error sintax

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -70,6 +70,6 @@ class TechnologyController extends Controller
 
         $technology->delete();
 
-        return to_route('admin.type.index')->with('delete_success', $technology);
+        return to_route('admin.technologies.index')->with('delete_success', $technology);
     }
 }
